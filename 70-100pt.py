aba29e70fca5a0f8376441840cb47adbdab2ea04
GrandestJax <491659@student.roundrockisd.org>
2014-11-11 15:57:47 -0600

    80pt done

--- 70-100pt.py
+++ 70-100pt.py
@@ -10,13 +10,13 @@ from Tkinter import *
 root = Tk()
 drawpad = Canvas(root, width=800,height=600, background='white')
 x1 = 390
-x2 =410
+x2 = 410
 y1 = 580
 y2 = 600
 player = drawpad.create_oval(x1,y1,x2,y2, fill="blue")
 enemy = drawpad.create_rectangle(50,50,100,60, fill="red")
 direction = 5
-
+direction1 = 5
 class myApp(object):
     def __init__(self, parent):
         
@@ -41,7 +41,6 @@ class myApp(object):
         drawpad.pack()
         root.bind_all('<Key>', self.key)
         self.animate()
-    
     def animate(self):
         global drawpad
         global enemy
@@ -56,19 +55,20 @@ class myApp(object):
 
     def key(self,event):
         global player
+        global direction1
+        x1,y1,x2,y2 = drawpad.coords(player)
         if event.char == "w":
-            drawpad.move(player,0,-10)
+             if y2 > 10:
+                drawpad.move(player,0,-direction1)
         if event.char == "a":
-            drawpad.move(player,-10,0)
+             if x2 > 10:
+                drawpad.move(player,-direction1,0)
         if event.char == "d":
-            drawpad.move(player,10,0)
+             if x1 < 790:
+                drawpad.move(player,direction1,0)
         if event.char == "s":
-            drawpad.move(player,0,10)
-        if event.char == "o":
-            rx1,ry1,rx2,ry2 = drawpad.coords(player)
-            Rocket = drawpad.create_rectangle(rx1,ry2,rx2,ry1, fill="red")
-            drawpad.move(Rocket,0,-20)
-            drawpad.after(20,self.key)
+             if y1 < 590:
+                drawpad.move(player,0,direction1)
     def collisionDetect(self,rocket):
         rx1,ry1,rx2,ry2 = drawpad.coords(rocket)
         
